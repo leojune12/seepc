1febacf2a9a435a69c1fceacad1eac5b5250cea7
ljbdu <leojunebedeo@gmail.com>
2021-03-19 01:32:32 +0800

    Update app.blade.php
    Add meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'seePC') }}</title>
+
+        <meta name="og:title" content="seePC">
+        <meta name="og:description" content="Publish your PC. Let other developers see your work station.">
+        <meta name="og:image" content="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/LCM%2BL_Working_Apple_I.jpg/800px-LCM%2BL_Working_Apple_I.jpg">
+        <meta name="og:url" content="https://seepc.herokuapp.com">
 
         <!-- Fonts -->
         {{--<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">--}}
